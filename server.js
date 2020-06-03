c26896cfd4e167f7af2ea7390807e357e8be9b4f
Robert Gannon <cyberpeaches@gmail.com>
2020-06-04 00:29:12 +0100

    Heroku needs ssl cert so back to http1.1

--- server.js
+++ server.js
@@ -2,41 +2,17 @@ const express     = require('express');
 const bodyParser  = require('body-parser')
 const MongoClient = require('mongodb').MongoClient;
 const mongourl    = require('./mongourl');
+const cors        = require('cors');
 const app         = express();
 
-var cors = require('cors');
-const fs = require('fs')
-const path = require('path')
-const spdy = require('spdy');
-const PORT = process.env.PORT || 3000;
 
-// created my own certificate with openssl for development
-const options = {
-  key: fs.readFileSync(path.join(__dirname, '/privateKey.key')),
-  cert: fs.readFileSync(path.join(__dirname, '/certificate.crt'))
-}
+const PORT        = process.env.PORT || 3000;
 
-console.log(options)
-
-  spdy
-    .createServer(options, app)
-    .listen(PORT, (error) => {
-      if (error) {
-        console.error(error)
-        return process.exit(1)
-      } else {
-        console.log('Listening on port: ' + PORT + '.')
-      }
-    })
-
-
-/*
   app.listen(PORT, () => {
       console.log(`Our app is running on port ${ PORT }`);
   });
-*/
 
-  MongoClient.connect(process.env.MONGODB_URI || 'mongodb+srv://RobTheThief:JoeMamma69@cluster0-cvv9k.mongodb.net/test?retryWrites=true&w=majority', {
+  MongoClient.connect(process.env.MONGODB_URI || mongourl, {
     useUnifiedTopology: true
   })
   .then(client => {
@@ -62,24 +38,35 @@ console.log(options)
   })
   .catch(error => console.error(error));
 
+/*Below is code that needs to be changed out for running the server
+  both local and HTTP/2; Heroku required ssl cert so 1.1 was chosen
+////////////////////////////////////////////////////////////////////
 
-  //////////////////////////////////
-  /*app.listen(3000, function() {
-    console.log('Listening on 3000');
-  })
+  const fs = require('fs')
+  const path = require('path')
+  const spdy = require('spdy');
+
+
+  // created my own certificate with openssl for development
+  const options = {
+    key: fs.readFileSync(path.join(__dirname, '/privateKey.key')),
+    cert: fs.readFileSync(path.join(__dirname, '/certificate.crt'))
+  }
+
+    spdy
+      .createServer(options, app)
+      .listen(PORT, (error) => {
+        if (error) {
+          console.error(error)
+          return process.exit(1)
+        } else {
+          console.log('Listening on port: ' + PORT + '.')
+        }
+      })
+^^^^^^^^^^^^HTTP/2
 
   MongoClient.connect(mongourl.url, {
     useUnifiedTopology: true
   })
   //^^^^^^^^^^^^^local
-
-  MongoClient.connect(process.env.MONGODB_URI || 'mongodb+srv://Get user from mongourl.js:Get password from mongourl.js@cluster0-cvv9k.mongodb.net/test?retryWrites=true&w=majority', {
-    useUnifiedTopology: true
-  })
-  *<<<<<<<<<<<<<<<<<
-  // ^^^^^^^altered app.listen and MongoClient.connect for Heroku test. Test ok. Reverting back to local build to complete dev
-
-  /*app.listen(3000, function() {
-    console.log('Listening on 3000');
-  })
   */
